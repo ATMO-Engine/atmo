e98544533b5ca703169b6cb0ff9452aa897f3670
Kapsulon <theo.bary@epitech.eu>
2025-03-11 19:00:30 +0100

    [fix] only format source code

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include <SDL3/SDL.h>
+#include <flecs.h>
 #include <glaze/glaze.hpp>
 #include <imgui.h>
-#include <flecs.h>
 #include <spdlog/spdlog.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     flecs::world ecs;
-    SDL_Window *window;                    // Declare a pointer
+    SDL_Window *window; // Declare a pointer
     bool done = false;
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+    SDL_Init(SDL_INIT_VIDEO); // Initialize SDL3
 
     // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL3 window",                  // window title
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
+    window = SDL_CreateWindow("An SDL3 window", // window title
+                              640, // width, in pixels
+                              480, // height, in pixels
+                              SDL_WINDOW_OPENGL // flags - see below
     );
 
     // Check that the window was successfully created
