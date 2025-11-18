7e08f738c0cab49bb0748579cbddfb12640b3909
Raphael Castres <raphael.castres@epitech.eu>
2025-11-18 11:44:06 +0900

    [add] thread safe pool

--- src/core/resource/pool.hpp
+++ src/core/resource/pool.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -21,19 +22,31 @@ namespace atmo
                 Pool();
                 ~Pool();
 
+                Pool(const Pool&) = delete;
+                Pool& operator=(const Pool&) = delete;
+
+                Pool(Pool&&) noexcept = default;
+                Pool& operator=(Pool&&) noexcept = default;
+
+
                 const Handle create(const std::string &path);
                 std::shared_ptr<Resource> getFromHandle(const Handle &handle);
 
                 void declareHandle(const Handle &handle);
-                void destroy(const Handle &handle);
                 void clear();
+                void clearHandle(const Handle &handle);
             private:
+                void destroy(const Handle &handle);
+
                 std::vector<std::string> m_usedHandles; // when adding a handle to this vecteur, add the ".path" value of the handle
                 std::unordered_map<std::string, Handle> m_handles;
 
                 std::vector<std::shared_ptr<Resource>> m_resources;
                 std::vector<std::uint16_t> m_generations;
                 std::vector<std::uint16_t> m_freeList;
+
+                std::unique_ptr<std::mutex> m_resourceMutex;
+                std::unique_ptr<std::mutex> m_handleManagementMutex;
             };
         } // namespace resource
     } // namespace core
