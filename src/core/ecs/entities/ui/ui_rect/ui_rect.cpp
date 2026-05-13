71c2a91fbbc7f525ba3d43c0a0cb1ba695d647a8
albe2 <alberic.flour-de-saegher@epitech.eu>
2026-05-13 14:21:52 +1000

    [add] uiRect call back link with engine event

--- src/core/ecs/entities/ui/ui_rect/ui_rect.cpp
+++ src/core/ecs/entities/ui/ui_rect/ui_rect.cpp
@@ -1,4 +1,7 @@
 #include "ui_rect.hpp"
+#include <iostream>
+#include "clay.h"
+#include "core/event/events/ui_event/hover_event/hover_event.hpp"
 #include "meta/auto_register.hpp"
 #include "spdlog/spdlog.h"
 
@@ -27,10 +30,29 @@ namespace atmo::core::ecs::entities
         d.backgroundColor = (rect.color * ui.modulate).toFloat<Clay_Color>(255);
         d.cornerRadius = { rect.corner_radius.top_left, rect.corner_radius.top_right, rect.corner_radius.bottom_left, rect.corner_radius.bottom_right };
 
+        event::EventRegistry::SetCallBack<event::events::HoverEvent>(
+            [ui](event::events::HoverEvent *event) { std::cout << "UI element ID from event: " << ui.element_id.id << std::endl; });
         return d;
     }
 
-    void UIRect::draw(ClaySdL3RendererData *data) {}
+    void HoverCallBack(Clay_ElementId id, Clay_PointerData data, intptr_t userData)
+    {
+        auto *rect = reinterpret_cast<components::UIRect *>(userData);
+
+        if (data.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
+            rect->color = types::Color{ static_cast<uint8_t>(255), static_cast<uint8_t>(100), static_cast<uint8_t>(0), static_cast<uint8_t>(255) };
+        }
+
+        auto default_event = atmo::core::event::EventRegistry::Create<atmo::core::event::events::HoverEvent>("Event::UIEvent::HoverEvent");
+        atmo::core::event::EventRegistry::Dispatch(default_event);
+    }
+
+    void UIRect::draw(ClaySdL3RendererData *data)
+    {
+        auto &rect = getComponentMutable<components::UIRect>();
+
+        Clay_OnHover(HoverCallBack, reinterpret_cast<intptr_t>(&rect));
+    }
 } // namespace atmo::core::ecs::entities
 
 ATMO_REGISTER_ENTITY(entities::UIRect);
