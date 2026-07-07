6b7c301e50ac90374bd6b698b3fe26a2dde53001
Kapsulon <theo.bary@epitech.eu>
2026-07-07 08:30:31 +0200

    [add] velocity in dynamic bodies

--- src/core/ecs/entities/2d/physics_2d/body_2d/dynamic_2d/dynamic_2d.cpp
+++ src/core/ecs/entities/2d/physics_2d/body_2d/dynamic_2d/dynamic_2d.cpp
@@ -3,7 +3,35 @@
 
 namespace atmo::core::ecs::entities
 {
-    void Dynamic2d::RegisterSystems(flecs::world *world) {}
+    void Dynamic2d::RegisterSystems(flecs::world *world)
+    {
+        world->system<components::Transform2d, Body2dData, Dynamic2dData>("Dynamic2d_UpdateValuesFromPhysicsEngine")
+            .kind(flecs::PostUpdate)
+            .each([](flecs::entity e, components::Transform2d &t, Body2dData &bd, Dynamic2dData &kd) {
+                if (!b2Body_IsValid(bd.body_id))
+                    return;
+
+                kd.linear_velocity = b2Body_GetLinearVelocity(bd.body_id);
+                kd.angular_velocity = atmo::common::math::RadiansToDegrees(b2Body_GetAngularVelocity(bd.body_id));
+            });
+
+        world->system<Body2dData, Dynamic2dData>("Dynamic2d_PushVelocityToPhysicsEngine")
+            .kind(flecs::OnValidate)
+            .each([](flecs::entity e, Body2dData &bd, Dynamic2dData &kd) {
+                if (!b2Body_IsValid(bd.body_id))
+                    return;
+
+                if (kd.linear_velocity != kd.synced_linear_velocity) {
+                    b2Body_SetLinearVelocity(bd.body_id, kd.linear_velocity);
+                    kd.synced_linear_velocity = kd.linear_velocity;
+                }
+
+                if (kd.angular_velocity != kd.synced_angular_velocity) {
+                    b2Body_SetAngularVelocity(bd.body_id, common::math::RadiansToDegrees(kd.angular_velocity));
+                    kd.synced_angular_velocity = kd.angular_velocity;
+                }
+            });
+    }
 
     void Dynamic2d::initialize()
     {
--- src/core/ecs/entities/2d/physics_2d/body_2d/dynamic_2d/dynamic_2d.hpp
+++ src/core/ecs/entities/2d/physics_2d/body_2d/dynamic_2d/dynamic_2d.hpp
@@ -24,6 +24,11 @@ namespace atmo::core::ecs::entities
         void setBodyType() override;
 
         struct Dynamic2dData {
+            types::Vector2 linear_velocity;
+            types::Vector2 synced_linear_velocity;
+
+            float angular_velocity = 0.0f;
+            float synced_angular_velocity = 0.0f;
         };
     };
 } // namespace atmo::core::ecs::entities
